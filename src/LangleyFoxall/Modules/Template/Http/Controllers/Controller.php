35e9f9a8f7742e1c4e4d0a68379971543fe1a5c7
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2018-05-01 15:02:06 +0100

    Copy default laravel controller

--- src/LangleyFoxall/Modules/Template/Http/Controllers/Controller.php
+++ src/LangleyFoxall/Modules/Template/Http/Controllers/Controller.php
@@ -2,9 +2,12 @@
 
 namespace LangleyFoxall\Modules\Template;
 
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class Controller
+class Controller extends BaseController
 {
-
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
