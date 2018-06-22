d5d14ea0caf69920b20fe1888f377b95b1c169c1
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2018-06-22 13:27:25 +0100

    Update Module.php

--- src/LangleyFoxall/Modules/Facade/Module.php
+++ src/LangleyFoxall/Modules/Facade/Module.php
@@ -4,6 +4,33 @@ namespace LangleyFoxall\Modules\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Module
+ * 
+ * @method static Module[] getModules()
+ * @method static Module   getModule(string $name)
+ *
+ * @method static Module[] getSubModules()
+ * @method static array    getSubModule(string $name)
+ *
+ * @method static Widget[] getWidgets()
+ * @method static array    getWidget(string $name)
+ *
+ * @method static Module createModule(string $name)
+ * @method static Module create(string $name)
+ *
+ * @method static bool deleteModule(string $name)
+ * @method static bool deleteSubModule(string $name)
+ * @method static bool deleteWidget(string $name)
+ *
+ * @method static bool hasModule(string $name)
+ * @method static bool hasSubmodule(string $name)
+ * @method static bool hasSubModules(string $name, bool $throw)
+ * @method static bool hasWidget(string $name)
+ * @method static bool hasWidgets(string $name, bool $throw)
+ * 
+ * @package LangleyFoxall\Modules\Facade
+ */
 class Module extends Facade
 {
 	protected static function getFacadeAccessor()
